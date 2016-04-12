eece8457fcaa7220c3115b5f699996b5278531b3
Jarrad Henry <jarrad.henry@gmail.com>
2016-04-12 14:06:04 -0700

    changing folder structure

--- routes/logout.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-	app.get('/logout', function(req, res){
-	  req.logout();
-	  req.flash('message', 'You have been log out successfully.')
-	  res.redirect('/login');
-	});
-}
--- util/util.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(req, res, next) {
-	// if(!req.isAuthenticated()) {
-	// 	console.log('Not logged in')
-	// 	req.flash('message', 'Please log in to continue.')
-	// 	return res.redirect('/login')
-	// }
-	next()
-}
