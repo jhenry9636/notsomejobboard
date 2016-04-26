bccfe863e6f98782b267acdda0f642a5ef93ec16
Jarrad Henry <jarrad.henry@gmail.com>
2016-04-26 02:16:54 -0700

    heroku

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ var expressSession = require('express-session');
 var nodeMailer = require('nodemailer');
 var expressHandlebars  = require('express-handlebars');
 
-var CandidateModel = require('./server/models/developer.model.js')();
-var RecruiterModel = require('./server/models/recruiter.model.js')();
-var ReviewModel = require('./server/models/review.model.js')();
-var ContactModel = require('./server/models/contact.model.js')();
+var CandidateModel = require(__dirname + '/server/models/developer.model.js')();
+var RecruiterModel = require(__dirname + '/server/models/recruiter.model.js')();
+var ReviewModel = require(__dirname + '/server/models/review.model.js')();
+var ContactModel = require(__dirname + '/server/models/contact.model.js')();
 
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(cookieParser());
@@ -30,42 +30,42 @@ server.use(expressSession({
 
 server.use(flash());
 server.use(express.static(path.join(__dirname, '/public')));
-var authenticationCheck = require('./server/common/authCheck.js');
+var authenticationCheck = require(__dirname + '/server/common/authCheck.js');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-require('./server/config/mongoose.config.js')(env, mongoose)
-require('./server/config/passport.config.js')(server, passport, CandidateModel, RecruiterModel, nodeMailer)
-require('./server/config/views.config.js')(server, expressHandlebars, path)
-require('./server/routes/static.route.js')(server)
+require(__dirname + '/server/config/mongoose.config.js')(env, mongoose)
+require(__dirname + '/server/config/passport.config.js')(server, passport, CandidateModel, RecruiterModel, nodeMailer)
+require(__dirname + '/server/config/views.config.js')(server, expressHandlebars, path)
+require(__dirname + '/server/routes/static.route.js')(server)
 
-var candidateCtrl = require('./server/controllers/developer.ctrl.js')(CandidateModel, passport, nodeMailer)
-var candidateRouter = require('./server/routes/developer.route.js')(candidateCtrl)
+var candidateCtrl = require(__dirname + '/server/controllers/developer.ctrl.js')(CandidateModel, passport, nodeMailer)
+var candidateRouter = require(__dirname + '/server/routes/developer.route.js')(candidateCtrl)
 server.use('/api/candidate', authenticationCheck, candidateRouter)
 
-var recruiterCtrl = require('./server/controllers/recruiter.ctrl.js')(RecruiterModel, passport, nodeMailer)
-var recruiterRouter = require('./server/routes/recruiter.route.js')(recruiterCtrl)
+var recruiterCtrl = require(__dirname + '/server/controllers/recruiter.ctrl.js')(RecruiterModel, passport, nodeMailer)
+var recruiterRouter = require(__dirname + '/server/routes/recruiter.route.js')(recruiterCtrl)
 server.use('/api/recruiter', authenticationCheck, recruiterRouter)
 
-var reviewRouter = require('./server/routes/reviews.route.js')(ReviewModel)
+var reviewRouter = require(__dirname + '/server/routes/reviews.route.js')(ReviewModel)
 server.use('/api/reviews', authenticationCheck, reviewRouter)
 
-var loginRouter = require('./server/routes/login.route.js')(passport)
+var loginRouter = require(__dirname + '/server/routes/login.route.js')(passport)
 server.use('/login', loginRouter)
 
 // Log Out Route
-require('./server/routes/logout.route.js')(server)
+require(__dirname + '/server/routes/logout.route.js')(server)
 
-var fakerRouter = require('./server/routes/faker.route.js')(CandidateModel, RecruiterModel, ContactModel)
+var fakerRouter = require(__dirname + '/server/routes/faker.route.js')(CandidateModel, RecruiterModel, ContactModel)
 server.use('/faker', fakerRouter)
 
-var verifyRouter = require('./server/routes/verify.route.js')(CandidateModel, RecruiterModel)
+var verifyRouter = require(__dirname + '/server/routes/verify.route.js')(CandidateModel, RecruiterModel)
 server.use('/verify', verifyRouter)
 
-var dashboardRouter = require('./server/routes/dashboard.route.js')(CandidateModel, RecruiterModel, passport)
+var dashboardRouter = require(__dirname + '/server/routes/dashboard.route.js')(CandidateModel, RecruiterModel, passport)
 server.use('/dashboard', authenticationCheck, dashboardRouter)
 
-require('./server/routes/contact.route.js')(server, ContactModel, CandidateModel, authenticationCheck)
+require(__dirname + '/server/routes/contact.route.js')(server, ContactModel, CandidateModel, authenticationCheck)
 
 
 var port = process.env.PORT || 8080;
