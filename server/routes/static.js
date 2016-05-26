52cd697ced9e56d7728cb6f3422674f4fab40b99
Jarrad Henry <jarrad.henry@gmail.com>
2016-05-25 21:34:37 -0700

    ui developers

--- server/routes/static.js
+++ server/routes/static.js
@@ -5,9 +5,20 @@ module.exports = function(server) {
     res.render('index')
   })
 
-  server.get('/developer/login', function(req, res) {
-    res.render('dev')
+  server.get('/developer/signup', function(req, res) {
+    res.render('developer.new.handlebars')
   })
+
+  server.get('/developer/login', function(req, res) {
+    res.render('developer.login.handlebars')
+  })
+
+  server.get('/developer/dashboard', function(req, res) {
+    res.render('developer.dashboard.handlebars', {
+      user: req.user
+    })
+  })
+
 
   server.get('/', function(req, res) {
 
