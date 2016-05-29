4b81a86056a89e8e315f1cd918c0c031290c18bf
Jarrad Henry <jarrad.henry@gmail.com>
2016-05-29 03:14:58 -0700

    get started widget

--- public/wizard/wizard.module.js
+++ public/wizard/wizard.module.js
@@ -1,5 +1,5 @@
 (function () {
   'use strict'
 
-  angular.module('nsj.slider', [])
+  angular.module('nsj.wizard', [])
 })()
---
+++ public/wizard/wizard.service.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict'
+
+  angular.module('nsj.wizard')
+    .factory('wizardService', wizardService)
+
+  function wizardService() {
+    
+    return service
+  }
+})()
