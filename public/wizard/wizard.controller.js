4cc3ebe2cb7dde6f142fb72546be5cf809f014a6
Jarrad Henry <jarrad.henry@gmail.com>
2016-05-29 17:17:48 -0700

    wizard

--- public/wizard/wizard.controller.js
+++ public/wizard/wizard.controller.js
@@ -4,7 +4,7 @@
   angular.module('nsj.wizard')
     .controller('WizardController', WizardController)
 
-  function WizardController() {
+  function WizardController($scope) {
     var vm = this;
 
     vm.currentStep = 'location';
@@ -12,6 +12,10 @@
     vm.hasComp = false;
     vm.hasTech = false;
     vm.hasProjects = false;
+    vm.shouldShowIndicator = false;
+    vm.goToLocation = goToLocation;
+    vm.goToComp = goToComp;
+    vm.goToTech = goToTech;
     vm.setLocation = setLocation;
     vm.setComp = setComp;
     vm.setTech = setTech;
@@ -23,21 +27,41 @@
     function setLocation() {
       vm.hasLocation = true;
       vm.currentStep = 'tech';
+      vm.shouldShowIndicator = true;
+      $scope.setLocationProgress();
     }
 
     function setComp() {
       vm.hasComp = true;
       vm.currentStep = 'creds';
+      $scope.setCompProgress();
     }
 
     function setTech() {
       vm.hasTech = true;
       vm.currentStep = 'comp';
+      $scope.setTechProgress();
     }
 
+    function goToLocation() {
+      vm.hasLocation = false;
+      vm.hasTech = false;
+      vm.hasComp = false;
+      vm.currentStep = 'location';
+      $scope.setWidth('0%')
+    }
 
-    function setCurrentSlide(slideName) {
+    function goToTech() {
+      vm.hasTech = false;
+      vm.hasComp = false;
+      vm.currentStep = 'tech';
+      $scope.setWidth('25%')
+    }
 
+    function goToComp() {
+      vm.hasComp = false;
+      vm.currentStep = 'comp';
+      $scope.setWidth('50%')
     }
 
   }
--- public/wizard/wizard.directive.js
+++ public/wizard/wizard.directive.js
@@ -7,10 +7,37 @@
   function nsjWizard() {
 
     return {
+      link: link,
       scope: true,
       templateUrl: '/wizard/wizard.html',
       controller: 'WizardController',
       controllerAs: 'vm'
+    }
+
+    function link($scope, element, attrs, controllers) {
+
+      $scope.setLocationProgress = setLocationProgress
+      $scope.setWidth = setWidth;
+      $scope.setTechProgress = setTechProgress
+      $scope.setCompProgress= setCompProgress
+
+      function setWidth(width) {
+        element.find('#wizard-progress').css({
+          width: width
+        })
+      }
+
+      function setLocationProgress() {
+        setWidth('25%')
+      }
+
+      function setTechProgress() {
+        setWidth('50%')
+      }
+
+      function setCompProgress() {
+        setWidth('75%')
+      }
     }
 
 
